4ab2a648387376f8786cf573597fec0c5210a8f4
Gary Mawdsley <79173+gmawdo@users.noreply.github.com>
2019-05-22 17:38:31 +0100

    New structure to being added to lasmaster

--- Restructure/lasmaster/file.py
+++ Restructure/lasmaster/file.py
@@ -1,5 +1,20 @@
 from laspy.file import File
 
+# Governs the interaction with laspy
+
+def std_data_types():
+	output = 	{
+			"p0"	:	(lambda x, y, z: x/(x+y+z)),
+			"p1"	:	(lambda x, y, z: y/(x+y+z)),
+			"p2"	:	(lambda x, y, z: z/(x+y+z)),
+			"eig0"	:	(lambda x, y, z: x),
+			"eig1"	:	(lambda x, y, z: y),
+			"eig2"	:	(lambda x, y, z: z),
+			"iso"	:	(lambda x, y, z: (x+y+z)/np.sqrt(3*(x**2+y**2+z**2))),
+			"ent"	:	(lambda x, y, z: entropy(np.stack((x, y, z))/(x+y+z))),
+			"rank"	:	(lambda x, y, z: 1*(x>thresh)+1*(y>thresh)+1*(z>thresh)),
+			}
+
 # U S E   L A S P Y   T O   T U R N   A   L A S   F I L E   I N T O
 # A   D I C T I O N A R Y   O F   A T T R I B U T E S
 # A N D   A   H E A D E R
