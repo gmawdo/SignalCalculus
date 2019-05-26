2537b219b26c4382051e6864e9e70b46f6fd88a7
Gary Mawdsley <79173+gmawdo@users.noreply.github.com>
2019-05-26 08:16:57 +0100

    converted to python3 and tweaked decimal places and chart ranges in order to gain insight

--- pylon.py
+++ pylon.py
@@ -7,15 +7,16 @@ import random
 import datetime
 from laspy.file import File
 
-#inFile = File("Tile9050nbrsRadius00_75thresh0_001vSpeed02_00dec00_10NFLClip100_00.las", mode = "r")
-inFile = File("attrTile9N006k050radius00_50thresh0_001v_speed02_00dec00_10.las", mode = "r")
+inFile = File("attrTile9NFLClip100_00N006k050radius00_50thresh0_001v_speed02_00dec00_10.las", mode = "r")
 
 # inFile.x
 # create matrix of all the eigenvectors
-result1 = np.stack(inFile.iso,inFile.lang), axis=-1)
-print "formed main result set size:",len(result1)
+#print(max(np.round(inFile.lang,0)),min(np.round(inFile.lang)))
+result1 = np.stack((np.round(inFile.iso,3),np.round(inFile.lang,2)), axis=-1)
+print("formed main result set size:",len(result1))
+print(result1.shape)
 
-# find how many unique isotropiesin combo with linear angles
+# find how many unique isotropies in combo with linear angles
 u1,i1,c1 = np.unique(result1, axis=0,return_inverse=True,return_counts=True)
 c1 = c1.reshape(u1.shape[0],1)
 u1 = np.concatenate((u1,c1),axis=1)
@@ -24,36 +25,36 @@ u1 = np.concatenate((u1,c1),axis=1)
 #u1 = u1[condition]
 u1.sort
 # NOW eigen, entropy, cnt
-print "form unqiue result set of distinct eignevector and entropy combos with counts",u1.shape
-print u1.shape #total number of tuples
+print("form unqiue result set of distinct isotropies and langs combos with counts",u1.shape)
+print(u1.shape) #total number of tuples
 
 # for info purposes get unqiue number of isotropies and unique number of linear angles
 u2,i2,c2 = np.unique(u1[:,[0]], axis=0,return_inverse=True,return_counts=True)
 u3,i3,c3 = np.unique(u1[:,[1]], axis=0,return_inverse=True,return_counts=True)
-print "number of distinct isotropies:",u2.shape
+print("number of distinct isotropies:",u2.shape)
 u2.sort
-print "number of distinct linear angles:",u3.shape
+print("number of distinct linear angles:",u3.shape)
 
 #isotropies,linear angle, cnt
 x, y, z = u1[:,0], u1[:,1], u1[:,2]
 
 #classify a set of points into a new file
-outFile = File("Gary-pylon.las", mode = "w", header = inFile.header)
-classification = inFile.classification
-outFile.points = inFile.points
-
-
-writecondition = (inFile.ent==0.000000001)
-classification[np.logical_not(writecondition)]=0
-
-classi = 10
-for s in u3[:,0]:
-	writecondition = (np.round(inFile.ent,3)==s)
-	classification[writecondition]=classi
-	classi = classi + 1
-
-outFile.classification = classification
-outFile.close()
+#outFile = File("Gary-pylon.las", mode = "w", header = inFile.header)
+#classification = inFile.classification
+#outFile.points = inFile.points
+#
+#
+#writecondition = (inFile.ent==0.000000001)
+#classification[np.logical_not(writecondition)]=0
+#
+#classi = 10
+#for s in u3[:,0]:
+#	writecondition = (np.round(inFile.ent,3)==s)
+#	classification[writecondition]=classi
+#	classi = classi + 1
+#
+#outFile.classification = classification
+#outFile.close()
 
 inFile.close()
 #PLOT
@@ -64,28 +65,29 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.view_init(elev=16, azim=43)
 
-ax.set_xlabel('eigenvector')
-ax.set_ylabel('Entropy')
+ax.set_xlabel('isotropy')
+ax.set_ylabel('lang')
 ax.set_zlabel('num points')
 
-#ax.set_xlim3d(0, 1800)
-#ax.set_ylim3d(0.0, 1.0)
-#ax.set_zlim3d(0, 750)
+ax.set_xlim3d(0.0, 1.0) #iso
+ax.set_ylim3d(0.0, 1.0) #lang
+ax.set_zlim3d(0, 15000) #numpoints
 
-ax.set_xlim3d(0, 200)
-ax.set_ylim3d(0.0, 0.03)
-ax.set_zlim3d(0, 500)
-
+#ax.set_xlim3d(0, 200)
+#ax.set_ylim3d(0.0, 0.03)
+#ax.set_zlim3d(0, 500)
 verts = []
 zs = u3[:,0]
 facecolors =[]
-for z in u3[:,0]:
-	ux = u1[(u1[:,1]==z)] #this predicate extracts entries only for this entropy value
-	xs = np.concatenate([[0],ux[:,0],[0]])
-	ys = np.concatenate([[0],ux[:,2],[0]])
-	#print xs,ys
-	verts.append(list(zip(xs,ys)))
-	facecolors.append((z, z * z, 0.0, 0.6))
+for z in u3[:,0]: #for each linear angle
+    ux = u1[(u1[:,1]==z)] #this predicate extracts entries only for this lang value
+    xs = np.concatenate([[0],ux[:,0],[0]]) #iso
+    ys = np.concatenate([[0],ux[:,2],[0]]) #num points
+    #print(xs)
+    #print("****")
+    #print(ys)
+    verts.append(list(zip(xs,ys)))
+    facecolors.append((z, z * z, 0.0, 0.6))
 
 poly = PolyCollection(verts, facecolors=facecolors)
 poly.set_alpha(0.7)
