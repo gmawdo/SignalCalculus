982bbb8aeec199f146a0df3bdc72e2d6e4b270bf
Gary Mawdsley <79173+gmawdo@users.noreply.github.com>
2019-05-29 00:14:12 +0100

    tweaked conductor detection code to explore eigenvector uids where high number of points

--- conductor.py
+++ conductor.py
@@ -24,7 +24,7 @@ def cart2sph(x,y,z):
     return ans
 
 #inFile = File("Tile9050nbrsRadius00_75thresh0_001vSpeed02_00dec00_10NFLClip100_00.las", mode = "r")
-inFile = File("attrTile9NFLClip100_00N006k050radius00_50thresh0_001v_speed02_00dec00_10.las", mode = "r")
+inFile = File("OLD-TEST-FILES/attrTile9NFLClip100_00N006k050radius00_50thresh0_001v_speed02_00dec00_10.las", mode = "r")
 
 
 print("file read")
@@ -37,8 +37,10 @@ print("formed main result set size:",len(result1))
 u1,i1,c1 = np.unique(result1, axis=0,return_inverse=True,return_counts=True)
 c1 = c1.reshape(u1.shape[0],1)
 u1 = np.concatenate((u1,c1),axis=1)
-#condition = (u1[:,2]>20) & (u1[:,2]<750)
-condition = (u1[:,1]<0.15)*(u1[:,2]>75)
+
+#condition = (u1[:,2]>50) & (u1[:,2]<750)
+#condition = (u1[:,1]<0.15)*(u1[:,2]>75)
+condition = (u1[:,1]<0.02)
 u1 = u1[condition]
 u1.sort
 # NOW eigen, entropy, cnt
@@ -48,6 +50,7 @@ print(u1.shape) #total number of tuples
 # for info purposes get unqiue number of EVs and unique number of entropies
 u2,i2,c2 = np.unique(u1[:,[0]], axis=0,return_inverse=True,return_counts=True) #ev
 u3,i3,c3 = np.unique(u1[:,[1]], axis=0,return_inverse=True,return_counts=True) #ent
+
 print("number of distinct EVs:",u2.shape)
 u2.sort
 print("number of distinct entropies:",u3.shape)
@@ -67,19 +70,17 @@ classification[np.logical_not(writecondition)]=0
 classi = 10
 for s in u3[:,0]:
     writecondition = (np.round(inFile.ent,3)==s)
-    classification[writecondition]=classi
+    if classi != 10:
+        classification[writecondition]=classi
     classi = classi + 1
-    print("complete class %i"%classi)
+    #print("complete class %i"%classi)
 
 outFile.classification = classification
 outFile.close()
 
 inFile.close()
-#PLOT
-#PLOT
-fig = plt.figure()
 
-#ax = fig.gca(projection='3d')
+fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.view_init(elev=16, azim=43)
 
@@ -87,31 +88,24 @@ ax.set_xlabel('eigenvector')
 ax.set_ylabel('Entropy')
 ax.set_zlabel('num points')
 
-#ax.set_xlim3d(0, 1800)
-#ax.set_ylim3d(0.0, 1.0)
-#ax.set_zlim3d(0, 750)
-
-ax.set_xlim3d(0, 200) #eigs
-ax.set_ylim3d(0.0, 0.03) #entropy
-ax.set_zlim3d(0, 500) #num points
+ax.set_xlim3d(0, 1800)
+ax.set_ylim3d(0.0, 1.0)
+ax.set_zlim3d(0, 100) #750
 
 verts = []
 zs = u3[:,0]
 facecolors =[]
 for z in u3[:,0]:
-	ux = u1[(u1[:,1]==z)] #this predicate extracts entries only for this entropy value
-	xs = np.concatenate([[0],ux[:,0],[0]]) #eig
-	ys = np.concatenate([[0],ux[:,2],[0]]) #num points
-	#print xs,ys
-	verts.append(list(zip(xs,ys)))
-	facecolors.append((z, z * z, 0.0, 0.6))
+    ux = u1[(u1[:,1]==z) & (u1[:,2]>20) ] #this predicate extracts entries only for this entropy value, second part of he predicate ensure we only consider eigenvectors that apply to more than 10 points
+    xs = np.concatenate([[0],ux[:,0],[0]]) #eig
+    ys = np.concatenate([[0],ux[:,2],[0]]) #num points
+    #print xs,ys
+    if z!=0: #this is the first entropy layer - equates to noise
+        print(ux[:,0],ux[:,2])
+    verts.append(list(zip(xs,ys)))
+    facecolors.append((z, z * z, 0.0, 0.6))
 
 poly = PolyCollection(verts, facecolors=facecolors)
 poly.set_alpha(0.7)
 ax.add_collection3d(poly, zs=zs, zdir='y')
-
-#X, Y = np.meshgrid(x, y, sparse=True)
-#Z = z.reshape(X.shape)
-#ax.plot_surface(X, Y, Z)
-
 plt.show()
