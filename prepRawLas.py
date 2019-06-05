c4ed8717a77bf75ef9feac679dbfdde620a27221
Gary Mawdsley <79173+gmawdo@users.noreply.github.com>
2019-06-05 12:41:46 +0100

    start to create reference set of signal plots

--- prepRawLas.py
+++ prepRawLas.py
@@ -1,5 +1,6 @@
 import os
 import lasmaster as la
-for filename in os.listdir():
-	if filename[:4]=="T200":
-		la.attr(la.nfl(filename))
+#for filename in os.listdir():
+#	if filename[:4]=="T200":
+#		la.attr(la.nfl(filename))
+la.attr("DF2000305_Completa.laz.las-GpsTime139962.95543662499999999974-139982.57958466666666666640.las")
