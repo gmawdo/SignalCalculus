ffc24ddd2ec041230c39ab914ce6a0e6f13f8148
Gary Mawdsley <79173+gmawdo@users.noreply.github.com>
2019-06-07 08:37:14 +0100

    saving some predicate exploration

--- prepRawLas.py
+++ prepRawLas.py
@@ -3,4 +3,4 @@ import lasmaster as la
 #for filename in os.listdir():
 #	if filename[:4]=="T200":
 #		la.attr(la.nfl(filename))
-la.attr("DF2000305_Completa.laz.las-GpsTime139236.86195908333333333332139256.48610712499999999998.las")
+la.attr(radius=0.5,file_name="DF2000305_Completa.laz.las-GpsTime139236.86195908333333333332139256.48610712499999999998.las")
