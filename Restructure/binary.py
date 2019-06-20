d96a0efc15a8471e1bcf6378c841e2b47ef4c354
Gary Mawdsley <79173+gmawdo@users.noreply.github.com>
2019-06-20 17:43:01 +0100

    kopt

--- Restructure/binary.py
+++ Restructure/binary.py
@@ -5,5 +5,7 @@ from sklearn.neighbors import NearestNeighbors
 import lasmaster as lm
 
 for file in os.listdir():
-	if file[:4] == "T200" and ("NFL" in file):
-		lm.wpd_attr(file)
+	if file[:4] == "attr" and ("NFL" in file):
+		inFile = File(file, mode = "rw")
+		inFile.intensity = 1000*inFile.entent
+		inFile.close()
