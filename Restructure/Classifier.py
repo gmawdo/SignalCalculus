dd7e1d5e80793e3f5b011ad77f3bda22c97ae1fa
Gary Mawdsley <79173+gmawdo@users.noreply.github.com>
2019-07-24 18:07:24 +0100

    More classifications - identified risks

--- Restructure/Classifier.py
+++ Restructure/Classifier.py
@@ -21,7 +21,6 @@ def corridor(c, conductor_condition, R=1, S=2):
 	v2 = inFile.eig21
 	v3 = inFile.eig22 # these are the three components of eigenvector 2
 	cRestrict =  c[:, conductor_condition]
-	print(cRestrict.shape)
 	nhbrs = NearestNeighbors(n_neighbors = 1, algorithm = "kd_tree").fit(np.transpose(cRestrict))
 	distances, indices = nhbrs.kneighbors(np.transpose(c))
 	nns = indices[:,0]
@@ -45,16 +44,17 @@ A = np.array([[1/3, 1/3, 1/3], [1,0,0], [0,1,0], [0,0,1], [0.5, 0, 0.5], [0,0.5,
 
 
 for file_name in os.listdir():	
-	if "k004_049radius00_50v_speed02_00" in file_name and file_name[:4]=="attr" and not("jsd" in file_name):
+	if "Infty" in file_name and file_name[:4]=="attr" and not("jsd" in file_name) and ("TILE19" in file_name):
 		inFile = File(file_name)
 		M = len(inFile)
 		B = np.stack((inFile.linearity, inFile.planarity, inFile.scattering), axis = -1) #(M,3)
 		Coords = np.vstack((inFile.x, inFile.y, inFile.z)) #(3,M)
 		unq, ind, inv, cnt = np.unique(np.round(Coords[:3,:]/2,0), return_index=True, return_inverse=True, return_counts=True, axis=1)
-		os.chdir("jsd")
+		os.chdir("OUTPUTS")
 		C = np.stack(tuple(np.broadcast_arrays(A[:,None,:], B[None,:, :])), axis = -1) #(7, M1, 3, 2)
 		JSD = jsd(C.transpose(0, 1, 3, 2)) #(7, M1)
 		dims = np.argmin(JSD, axis = 0)
+		
 		dims[inFile.reader.get_dimension("1dist")>0.5]=7
 		frame = {'A': inv,
 			str(0): (dims==0).astype(int),
@@ -67,7 +67,11 @@ for file_name in os.listdir():
 			}
 		df = pd.DataFrame(frame)
 		X = (df.groupby('A').sum()).values #(M1,3)
-		entropies = entropy(X/(np.sum(X, axis = 1)[:,None]))
+		inv_save = inv
+		
+		entropies = entropy(X/(np.sum(X, axis = 1)[:,None]))[inv]
+
+		#the below are dangerous -- we need a better QC
 		c = {arg: 1000*X[inv, arg] for arg in range(7)}
 		conditions = 	[
 						c[0] <= 10*cnt[inv],
@@ -78,15 +82,16 @@ for file_name in os.listdir():
 						c[5] <= 1*cnt[inv],
 						c[6] <= 10*cnt[inv],
 						]
-		classn = inFile.classification
+		classn = dims
 		classn[:] = 1
 		for item in conditions:
 			classn[np.logical_not(item)] = 0
 		dim1 = classn == 1
-		classn1 = classn[dim1]
+
+
 		clustering = DBSCAN(eps=0.5, min_samples=4).fit((Coords[:, dim1]).transpose(1,0))
 		labels = clustering.labels_
-		
+
 		frame =	{
 					'A': labels,
 					'X': inFile.x[dim1],
@@ -95,10 +100,10 @@ for file_name in os.listdir():
 		df = pd.DataFrame(frame)
 		maxs = (df.groupby('A').max()).values
 		mins = (df.groupby('A').min()).values
-		#print(mins.shape)
 		unq, ind, inv, cnt = np.unique(labels, return_index=True, return_inverse=True, return_counts=True)
 		lengths = (np.sqrt((maxs[:,0]-mins[:,0])**2+(maxs[:,1]-mins[:,1])**2))[inv]
 		lengths[labels==-1]=0
+		classn1 = classn[classn == 1]
 		classn1[lengths<=1]=0
 		classn[classn==1]=classn1
 
@@ -116,7 +121,6 @@ for file_name in os.listdir():
 		df = pd.DataFrame(frame)
 		maxs = (df.groupby('A').max()).values
 		mins = (df.groupby('A').min()).values
-		#print(mins.shape)
 		unq, ind, inv, cnt = np.unique(labels, return_index=True, return_inverse=True, return_counts=True)
 		lengths = (np.sqrt((maxs[:,0]-mins[:,0])**2+(maxs[:,1]-mins[:,1])**2))[inv]
 		lengths[labels==-1]=0
@@ -126,11 +130,38 @@ for file_name in os.listdir():
 
 		boolean_conductor = corridor(Coords, classn == 1, R=1, S=2)
 		classn[:] = 0
+		pylon = (np.sum(X[:,[1,2,6]], axis = 1)/np.sum(X, axis = 1))[inv_save] >0.9
+		classn[pylon & (inFile.ang2 < 0.2)] = 2
 		classn[boolean_conductor] = 1
 
+		nhbrs = NearestNeighbors(n_neighbors = 1, algorithm = "kd_tree").fit(np.transpose(Coords[:2, classn == 1]))
+		distances, indices = nhbrs.kneighbors(np.transpose(Coords[:2, classn == 2]))
+		z = inFile.z
+		zdist = z[classn == 2]-(z[classn==1][indices[:,0]])
+		classn2 = classn[classn == 2]
+		classn2[(distances[:,0]>0.5) | (zdist > 1)] = 0
+		clustering = DBSCAN(eps=1, min_samples=4).fit((Coords[:, classn==2]).transpose(1,0))
+		labels = clustering.labels_
+		unq, ind, inv, cnt = np.unique(labels, return_index=True, return_inverse=True, return_counts=True)
+		classn2[labels == -1] = 0
+
+
+		classn[classn == 2] = classn2
+		Pylon = corridor(Coords, classn == 2, 1, 4) 
+		classn[classn != 1] =0
+		classn[Pylon & np.logical_not(classn == 1)] = 2
+		
+		nhbrs = NearestNeighbors(n_neighbors = 1, algorithm = "kd_tree").fit(np.transpose(Coords[:, classn == 1]))
+		distances, indices = nhbrs.kneighbors(np.transpose(Coords[:, classn == 0]))
+		classn0 = classn[classn == 0]
+		classn0[distances[:,0] <= 0.5] = 4
+		classn[classn == 0] = classn0
+		classn[(classn == 4) & (entropies < 0.7)] = 0
 
 		out = File("Conductor"+file_name, mode = "w", header = inFile.header)
 		out.points = inFile.points
+
+	
 		out.classification = classn
 		out.close()
 		os.chdir("..")
