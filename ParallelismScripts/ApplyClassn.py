fa380352a7be495ac10e0a3548368c34379ca38a
Gary Mawdsley <79173+gmawdo@users.noreply.github.com>
2019-09-09 16:39:04 +0100

    Upgraded classn

--- ParallelismScripts/ApplyClassn.py
+++ ParallelismScripts/ApplyClassn.py
@@ -5,7 +5,6 @@ import os
 from sklearn.neighbors import NearestNeighbors
 from sklearn.cluster import DBSCAN
 import time
-from scipy.spatial import ConvexHull
 
 timingstxt = open("CLASSN_TIMINGS.txt", "w+")
 
@@ -15,7 +14,7 @@ def jsd(distribution):
 	return (entropy(np.mean(distribution, axis = -2))-np.mean(entropy(distribution), axis = -1))*np.log(N)/np.log(M)
 
 
-def corridor(c, conductor_condition, among, R=1, S=2):
+def corridor(c, conductor_condition, R=1, S=2):
 
 	v1 = inFile.eig20
 	v2 = inFile.eig21
@@ -29,7 +28,7 @@ def corridor(c, conductor_condition, among, R=1, S=2):
 	scale =(u[0,:]*v[0,:]+u[1,:]*v[1,:]+u[2,:]*v[2,:])
 	w = u-scale*v
 	w_norms = np.sqrt(w[0,:]**2+w[1,:]**2+w[2,:]**2)
-	condition = (w_norms<R) & (np.absolute(scale)<S) & among
+	condition = (w_norms<R)&(np.absolute(scale)<S)
 	
 	return condition
 
@@ -62,22 +61,22 @@ for tile_name in os.listdir():
 		dims = np.argmax(LPS, axis = 1)+1
 		dims[inFile.eig2<=0]=7
 
-		v0 = 1*inFile.eig20
-		v1 = 1*inFile.eig21
-		v2 = 1*inFile.eig22
-		condition = ((v0>=0)&(v1<0)&(v2<0))|((v1>=0)&(v2<0)&(v0<0))|((v2>=0)&(v0<0)&(v1<0))|((v0<0)&(v1<0)&(v2<0))
-		v0[condition]=-v0[condition]
-		v1[condition]=-v1[condition]
-		v2[condition]=-v2[condition]
-		v = np.vstack((5*v0,5*v1,5*v2, inFile.x, inFile.y, inFile.z))
-
 		if (dims == 1).any():
+			v0 = 1*inFile.eig20
+			v1 = 1*inFile.eig21
+			v2 = 1*inFile.eig22
+			condition = ((v0>=0)&(v1<0)&(v2<0))|((v1>=0)&(v2<0)&(v0<0))|((v2>=0)&(v0<0)&(v1<0))|((v0<0)&(v1<0)&(v2<0))
+			v0[condition]=-v0[condition]
+			v1[condition]=-v1[condition]
+			v2[condition]=-v2[condition]
+			v = np.vstack((5*v0,5*v1,5*v2, inFile.x, inFile.y, inFile.z))
 			clustering = DBSCAN(eps=0.5, min_samples=1).fit((v[:, dims == 1]).transpose(1,0))
 			labels = clustering.labels_
 			frame =	{
 				'A': labels,
 				'X': inFile.x[dims == 1],
 				'Y': inFile.y[dims == 1],
+				'Z': inFile.z[dims == 1]
 				}
 			df = pd.DataFrame(frame)
 			maxs = (df.groupby('A').max()).values
@@ -90,17 +89,83 @@ for tile_name in os.listdir():
 			classn1[:] = 1
 			classn1[lengths<=2]=0
 			classn[dims == 1]=classn1
+			conductor = corridor(Coords, classn == 1, R=0.5, S=2)
+			classn[conductor]=1
+			classn[dims == 7] = 7
+		
+		prepylon = (dims == 2)&(classn != 1)
+		if prepylon.any():
+			v0 = 1*inFile.eig00
+			v1 = 1*inFile.eig01
+			v2 = 1*inFile.eig02
+			condition = ((v0>=0)&(v1<0)&(v2<0))|((v1>=0)&(v2<0)&(v0<0))|((v2>=0)&(v0<0)&(v1<0))|((v0<0)&(v1<0)&(v2<0))
+			v0[condition]=-v0[condition]
+			v1[condition]=-v1[condition]
+			v2[condition]=-v2[condition]
+			v = np.vstack((5*v0,5*v1,5*v2, inFile.x, inFile.y, inFile.z))
+			clustering = DBSCAN(eps=0.5, min_samples=1).fit((v[:, prepylon]).transpose(1,0))
+			labels = clustering.labels_
+			frame =	{
+				'A': labels,
+				'X': inFile.x[prepylon],
+				'Y': inFile.y[prepylon],
+				'Z': inFile.z[prepylon]
+				}
+			df = pd.DataFrame(frame)
+			maxs = (df.groupby('A').max()).values
+			mins = (df.groupby('A').min()).values
+			unq, ind, inv, cnt = np.unique(labels, return_index=True, return_inverse=True, return_counts=True)
+			lengths = (np.sqrt((maxs[:,0]-mins[:,0])**2+(maxs[:,1]-mins[:,1])**2))[inv]
+			lengths[labels==-1]=0
+			classn2 = classn[prepylon]
+			classn2[:] = 2
+			classn2[lengths<=2]=0
+			classn[prepylon]=classn2
+			nhbrs = NearestNeighbors(n_neighbors = 1, algorithm = "kd_tree").fit(np.transpose(Coords[:, classn == 2]))
+			distances, indices = nhbrs.kneighbors(np.transpose(Coords))
+			classn[(distances[:,0]<0.5)& (classn != 7) & (classn != 1) ]=2
 
-		conductor = corridor(Coords, classn == 1, dims==1, R=0.5, S=2)
-		classn[:]=0
-		classn[conductor & (dims != 7)]=1
-		classn[dims == 7] = 7
-		
-		
+		preveg = (dims == 3) & (classn != 2) & (classn != 1)
+		if preveg.any():
+			v = np.vstack((inFile.x, inFile.y, inFile.z))
+			clustering = DBSCAN(eps=0.5, min_samples=1).fit((v[:, preveg]).transpose(1,0))
+			labels = clustering.labels_
+			frame =	{
+				'A': labels,
+				'X': inFile.x[preveg],
+				'Y': inFile.y[preveg],
+				'Z': inFile.z[preveg]
+				}
+			df = pd.DataFrame(frame)
+			maxs = (df.groupby('A').max()).values
+			mins = (df.groupby('A').min()).values
+			unq, ind, inv, cnt = np.unique(labels, return_index=True, return_inverse=True, return_counts=True)
+			lengths = (np.sqrt((maxs[:,0]-mins[:,0])**2+(maxs[:,1]-mins[:,1])**2))[inv]
+			lengths[labels==-1]=0
+			classn3 = classn[preveg]
+			classn3[:] = 3
+			classn3[lengths<2]=0
+			classn[preveg] = classn3
+			nhbrs = NearestNeighbors(n_neighbors = 1, algorithm = "kd_tree").fit(np.transpose(Coords[:, classn == 3]))
+			distances, indices = nhbrs.kneighbors(np.transpose(Coords))
+			classn[(distances[:,0]<0.5)& (classn != 7) & (classn != 1) & (classn != 2)]=3
+
+		nhbrs = NearestNeighbors(n_neighbors = 1, algorithm = "kd_tree").fit(np.transpose(Coords[:, (classn != 0) & (classn != 7)]))
+		distances, indices = nhbrs.kneighbors(np.transpose(Coords[:, classn == 0]))
+		classn0 = classn[classn == 0]
+		classn0[(distances[:,0]<0.5)] = (classn[(classn != 0) & (classn != 7)])[indices[(distances[:,0]<0.5),0]]
+		classn[(classn==0)] = classn0
+
+		if (classn == 1).any() and (classn == 3).any():
+			nhbrs = NearestNeighbors(n_neighbors = 1, algorithm = "kd_tree").fit(np.transpose(Coords[:, classn == 1]))
+			distances, indices = nhbrs.kneighbors(np.transpose(Coords[:, classn == 3]))
+			classn3 = classn[classn==3]
+			classn3[distances[:,0]<0.5]=4
+			classn[classn==3]=classn3
+
 		outFile = File("classified"+tile_name, mode = "w", header = inFile.header)
 		outFile.points = inFile.points
 		outFile.classification = classn
-		outFile.intensity = dims
 		outFile.close()
 		print(tile_name)
 
