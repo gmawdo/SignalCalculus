c879418f1e9a0dfe8d5dd1416f6cac181516f4f3
Gary Mawdsley <79173+gmawdo@users.noreply.github.com>
2019-10-02 16:48:36 +0100

    als_model and lm fix

--- qc/als_model.py
+++ qc/als_model.py
@@ -14,21 +14,18 @@ SR = 40.0 # scan rate, Hz, lines per second, LPS
 pulse_rate = 450000
 speed_kts = 110 # converted from 110 kts to m/s
 x_range = 100 # meters
-mode = "shm" # "shm" or "linear"
-density_mode = "radial" # "none" or "voxel" or "radial"
+mode = "sawtooth" # "shm" or "linear" or "sawtooth"
+density_mode = "voxel" # "none" or "voxel" or "radial" 
 scale = 1
 
-speed_mps = 0.514444*speed_kts
+speed_mps = np.round(0.514444*speed_kts,2)
 flight_time = x_range/speed_mps
 num_pts = int(np.ceil(flight_time*pulse_rate))
 times = np.arange(num_pts)*(1/pulse_rate)
-print("mode:", mode)
-print("density mode:", density_mode)
 print("num. of points", num_pts)
 print("pulse rate", pulse_rate)
-print("speed", speed_mps)
+print("speed", speed_mps, "(mps)")
 print("flight range", x_range)
-print("time elapsed", max(times)-min(times))
 print("scan rate", SR)
 print("field of view", FOV)
 
@@ -47,13 +44,15 @@ y = np.empty(num_pts, dtype = float)
 z = np.empty(num_pts, dtype = float)
 intensity = np.empty(num_pts, dtype = int)
 
-angular_amplitude = (FOV)*(np.pi/180) # formulas subject to change when more understanding gained
-frequency = SR # formulas subject to change when more understanding gained
+angular_amplitude = (FOV/2)*(np.pi/180) # formulas subject to change when more understanding gained
+frequency = SR*2 # formulas subject to change when more understanding gained
 
 if mode == "shm":
 	function = lambda x: np.cos(2*np.pi*x)
 if mode == "linear":
 	function = lambda x: (2*(2*x-np.floor(2*x))-1)*((-1)**((np.floor(2*x)).astype(int)))
+if mode == "sawtooth":
+	function = lambda x: 2*(x-np.floor(x))-1
 
 theta = angular_amplitude*function(times*frequency)
 
@@ -64,15 +63,17 @@ z[:] = 0
 coords = np.stack((x,y,z), axis = 1)
 differences = coords[:-1, :]-coords[1:,:]
 distances_across_track = np.sqrt(np.sum(differences**2, axis = 1))
-
-print("max pt spacing across track", np.max(distances_across_track))
+print("")
+print("max pt spacing across track", np.max(distances_across_track[distances_across_track<0.5*(max(x)-min(x))]))
 print("max pt spacing along track", speed_mps/frequency)
-print("swath width", max(x)-min(x))
-
+print("")
+print("swath width", np.round(max(x)-min(x),2))
+print("time elapsed", np.round(max(times)-min(times),2))
+print("")
 unq, ind, inv, cnt = np.unique((np.floor(coords)).astype(int), return_index=True, return_inverse=True, return_counts=True, axis=0)
-print("voxel mode lowest ppm", min(cnt), "(always given, even when radial selected)")
-print("voxel mode highest ppm", max(cnt), "(always given, even when radial selected)")
-print("voxel mode avg ppm", np.mean(cnt[inv]), "(always given, even when radial selected)")
+print("voxel mode lowest ppm", min(cnt))
+print("voxel mode highest ppm", max(cnt))
+print("voxel mode avg ppm", np.mean(cnt[inv]))
 unq1, ind1, inv1, cnt1 = np.unique((np.floor(coords/0.1)).astype(int), return_index=True, return_inverse=True, return_counts=True, axis=0)
 frame =	{
 		'A': inv,
@@ -80,11 +81,11 @@ frame =	{
 		}
 df = pd.DataFrame(frame)
 nunique_vals = ((df.groupby('A')['B'].nunique()).values)[inv]
-
+print("")
 print("lowest coverage", min(nunique_vals), "percent")
 print("highest coverage", max(nunique_vals), "percent")
-print("avg coverage", np.mean(nunique_vals), "percent")
-
+print("avg coverage", np.round(np.mean(nunique_vals),2), "percent")
+print("")
 if density_mode == "voxel":
 	ppm = cnt[inv]
 
@@ -117,31 +118,8 @@ newFile.x = x
 newFile.y = y
 newFile.z = z
 newFile.intensity = ppm
+newFile.classification = inv%32
 newFile.close()
 
-
-#num_x = int((max(x)-min(x))/scale)
-#num_y = int((max(y)-min(y))/scale)
-#x_grid = scale*(int(min(x)/scale)+np.arange(num_x))
-#y_grid = scale*(int(min(y)/scale)+np.arange(num_y))
-#mesh = np.meshgrid(x_grid, y_grid)
-
-#coords_grid = np.stack((mesh[0].flatten(), mesh[1].flatten(), np.zeros(mesh[0].size, dtype = float)), axis = 1)
-#tree = BallTree(coords)
-#radius = np.sqrt((scale/np.pi))
-#balls = tree.query_radius(coords_grid, r=radius)
-
-#grid = File("grid_als_model.las", mode = "w", header = newHeader)
-#grid.header.scale = [0.0001, 0.0001, 0.0001]
-#grid.x = coords_grid[:,0]
-#grid.y = coords_grid[:,1]
-#grid.z = coords_grid[:,2]
-#grid_intensity = np.zeros(coords_grid.shape[-2], dtype = int)
-#for item in range(coords_grid.shape[-2]):
-#	grid_intensity[item] = len(balls[item])
-#grid.intensity = grid_intensity
-
-#grid.close()
-
 end = time.time()
-print((end-start)/60, "minutes")
+print("ran in", np.round((end-start)/60,2), "minutes")
